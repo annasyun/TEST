0e3f397cf2d92011e77d6f73dd415d5ca1d9b1a1
annasyun <yunjbly2470@naver.com>
2023-03-15 18:06:44 +0900

    feat: style-test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let post = "인천 브런치 맛집";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 className={post}>react,,,master</h4>
+      </div>
+      <h4 style={{ color: "red" }}>{post}</h4>
     </div>
   );
 }
