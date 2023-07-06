04666b7fd4c838658d8544e45c86eab4abae240f
annasyun <yunjbly2470@naver.com>
2023-07-06 15:32:36 +0900

    project start

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+// import ReactDOM from 'react-dom'; //구버전
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(<App />);
